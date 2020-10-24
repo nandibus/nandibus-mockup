6071e6b6eddd08fa30b5d9de5f81514227e38ace
Douglas Valerio <douglas.valerio@matilha.digital>
2020-10-24 13:51:39 -0300

    generic click sound added to project

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 
 
 const nandibus = document.querySelector('.nandibus');
+const keysArray = document.querySelectorAll('.key');
 const nandibusWitdh = nandibus.clientWidth;
 
 const resizeNandibus = () => {
@@ -9,6 +10,30 @@ const resizeNandibus = () => {
     nandibus.style.transform = `scale(${scale})`;
 }
 
+const addEventListenersToKeys = ()=>{
+    Array.from(keysArray).forEach(function(element) {
+        element.addEventListener('click', playclip);
+      });
+}
+
 resizeNandibus();
+addEventListenersToKeys();
 
 window.addEventListener('resize', resizeNandibus);
+
+function playclip (){
+    if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
+    if (document.getElementById('nandibus'))
+     {
+      document.getElementById('nandibus').sound.src = "sounds/click.mp3";
+     }
+    }
+    
+    else {
+    {
+    var audio = document.getElementsByTagName("audio")[0];
+    audio.play();
+    }
+    }
+    }
+    

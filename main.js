4c334973e9057d5852c429f166fe72166896cd8d
Onelio de Marchi Junior <oneliojr@gmail.com>
2020-10-24 17:52:05 -0300

    Add light to NANDIBUS

--- main.js
+++ main.js
@@ -29,8 +29,25 @@ keys.forEach(key => key.addEventListener('click', playSound ));
 
 /* CLICK NANDIBUS KEY ON KEYPRESS */
 
+////
+
+const lights = Array.from(document.querySelectorAll('.nandibus-lights div'));
+
+const toogleLight = () => {
+
+    lights.forEach(light => {
+        light.classList.contains('--on') ?
+            light.classList.remove('--on') :
+            light.classList.add('--on');
+    });
+};
+
 window.addEventListener('keydown', e => {
     const key = keys.filter(key => key.dataset.keycode === e.code)[0];
+
+    if (e.code === 'KeyL') {
+        toogleLight();
+    }
 
     if (!key) { return; }
 
